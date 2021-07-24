ef11dfe147e43d6fc3234f68e49d1e611474708a
lenscaster <87354112+lenscaster@users.noreply.github.com>
2021-07-24 16:23:03 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,25 +1,21 @@
-/* Codded by @lenscaster
-
-Telegram: t.me/RavinduManoj
-Facebook: https://www.facebook.com/ravindu.manoj.79
+/* Copyright (C) 2020 lenscaster.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Whats bot - lenscaster
+WhatsAsena - Yusuf Usta
 */
 const fs = require('fs')
 const Asena = require('../events');
 const {MessageType, Mimetype } = require('@adiwajshing/baileys');
 const FilterDb = require('./sql/filters');
-const Config = require('../config');
-
+const Config = require('../config')
+const jid = Config.DISBGM != false ? Config.DISBGM.split(',') : [];
 const Language = require('../language');
 const Lang = Language.getString('filters');
-
+if (Config.WORKTYPE == 'private') {
 Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC, dontAddCommandList: true}, (async (message, match) => {
     match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
-
     if (match === null) {
         filtreler = await FilterDb.getFilter(message.jid);
         if (filtreler === false) {
@@ -37,13 +33,11 @@ Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC,
         await message.client.sendMessage(message.jid,Lang.FILTERED.format(match[0].replace(/['"]+/g, '')),MessageType.text);
     }
 }));
-
 Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, dontAddCommandList: true}, (async (message, match) => {
     match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
     if (match === null) {
         return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + '\n*Example:* ```.stop "hello"```',MessageType.text)
     }
-
     del = await FilterDb.deleteFilter(message.jid, match[0].replace(/['"“]+/g, ''));
     
     if (!del) {
@@ -52,18 +46,21 @@ Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, don
         await message.client.sendMessage(message.jid,Lang.DELETED, MessageType.text)
     }
 }));
-Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
-        if (!!message.mention && message.mention[0] == '9400616230@s.whatsapp.net') {
+Asena.addCommand({on: 'text', fromMe: false }, (async (message, match) => {
+    if(Config.BGMFILTER){
+        let banned = jid.find( Jid => Jid === message.jid);
+        if(banned !== undefined) return
+        if (!!message.mention && message.mention[0] == '919946432377@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/Mention.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
-const array = ['Helo','King','Kooi','Ramos','Tentacion','Neymar','umma','Music','Kurup','Rose','aara','Alone','ayilla','bie','Chiri','colony','enth','entha','Fuck','Goal','Hambada','Kanja','kuthirappavan','mathi','mier','moonji','pever','Potta','Serious','Soldier','Sry','Subscribe','thottu','Va','Vada','vimanam','sorry','nanban','Lala','Smile','ghost','La be','Sed','Uff','Legend','music','Fek','Psycho','Town','Pwoli','Uyir','Malang','Bad','Boss','Thamasha','big fan','gd n8','kar98','love u','Love u','Endi','endi','noob','Noob','Poweresh','Perfect ok','perfect ok','power','saji','single','waiting','Myr','myr','uyir','thug','avastha','Moodesh','sketched','Cr7','manasilayo','Hlo','Poda','Aarulle','Cr7 back','Portugal','ennitt','Haters','ayn','Kgf','Messi','Hehe','hehe','Venda','venda','chadhi','Chadhi','Hbday','hbday','Myre','myre','Oompi','oompi','parayatte','Fresh','fresh','Ok da','ok da','Feel aayi','scene','Ok bei','ok bei','Da','Kozhi','kozhi','adi','Adi','kali','Kali','thantha','Thantha','Aysheri','aysheri','thot','Thot','Sneham','pm','Pm','paatt','Paatt','njan','Njan','life','Life','Killadi','killadi','good bye','Good bye','evide','Evide','achan','Achan','kunna','Kunna','broken','Broken','why','Why','enth patti','Enth patti','pani','Pani','padicho','Padicho','paad','Paad','Chatho','chatho','lover','Lover','nanayikoode','Nanayikoode','die','hate','Hate','nallath','Nallath','Neymer','neymer','Njr','njr','patti','Patti','poora','Poora','Rohit','rohit','thall','Thall','Theri','theri','potte','Potte','Pinky','Caption','caption','onn poyi','Onn poyi','problem','Problem','Ameer','ameer','Suhail','Mathy','BGM','Die','Jd','Loveu','Veeran','Waiting','ano','ara','Ara','alone','bad boy','brokenlove','care','chunke','comedy','devadha','ekk','fd','free','gdmng','gdngt','group','ha','i am back','ijathi','jd','kadhal','kerivaa','kiss','kukku','line','lucifer','machan','Chetta','Per','massbgm','Mass','matam','may i','mindathe','my area','mylove','njn','padakam','polika','racal','rasool','rashmika','return','sneham','sulthan','thayirmulak','vada','wow','Thalapathy','Aliya','Leopucha','Poompatta gunda','Bgm','En nenjil','evde','Frnd','Game','Hy','Hoi','Yaar','Kadhali','king','Paavam','Pewer','pewer','Power','Pranayam','sed','Umma','Vava','Kunju','💪','🤣','Sis','Bairavaa','bgm','Love tune','Mohanlal','Singapenne','Single','Jocker','1 vs 1 vada','1','alive','Aliyo','Ardra','Area','Ayn','bot','Chathi','Chunk','Chunks','cr7','Cry','Dai','Dora','Entry bgm','Gd mng','Gd ngt','Hloo','Kanjan','Ikkachi','Kanjav','Kevin','Kundan','kundan','manath','messi','My love','Nalla kutty','Nanban','Nanni','Nishal','Njn vera','Oh','Ok','Over','Pala shaji','Pever','Pinnallah','Poli','Potte','Power varatte','Re entry','Sad','sad','T','Sahva','Scene','sed','Saji','Seth po','sis','Supper','Vidhi','Baby','Music pranthan','Kenzo','kenzo','Kenzoo','Ashkar','per','Hbd','Mad','Blackzue','Nallakutti','Enthada','Akhil','Love','Fsq','Nanbiye','Iblees','Rashmika','Kutty','Arakkal iblees','Althaf','Sayip','thamasha','Edi','Muth','Muthe','Omb','2','Mood','Ok bye','Eda','Track maat','Chaya kudicho','photo','Set','Poote','Chathy','Aara','ayye','Ayyo','Bass bgm','Bgm id','Bhasi','Biscket','Chirikano','Chunnk uyr','ok','Umbi','da','Csk','ee','Ellarum ede','Etha cheyya','Entha','Fan','ff','grp','Kanapi','Kanaran','Kemam','kk','left','Lo','Loo','Lub u','muthe','My god','oombi','over','Paat','Panni','Podai','Pooda','Pora','Poyeda','Rasheed','Sed aayi','Sed tune','sed','Super','thayoli','Thug','Vaa','nandu','Nandu','welcome','Me','Wait']
+const array = ['name entha','Helo','King','Kooi','Tuttu','Azaru','Ramos','Tentacion','baby','Loveee','Neymar','umma','Music','Kurup','Friend','Rose','aara','Alone','ayilla','bie','Chiri','colony','enth','entha','Fuck','Goal','Hambada','Kanja','Killedi','DD','kuthirappavan','mathi','Meeting','mier','moonji','Name','Oh no','pever','Potta','Serious','Soldier','Sry','Subscribe','thottu','Va','Vada','vimanam','sorry','nanban','Lala','Smile','ghost','La be','Sed','Uff','Legend','music','Fek','Psycho','Town','Pwoli','Uyir','Malang','Bad','Boss','Thamasha','big fan','charlie','gd n8','kar98','love u','Love u','Endi','endi','noob','Noob','Poweresh','Perfect ok','perfect ok','power','saji','single','waiting','Myr','myr','Malappuram','uyir','thug','avastha','Moodesh','sketched','Cr7','Z aayi','manasilayo','Hi','Hlo','Poda','nirtheda','Aarulle','Cr7 back','Portugal','ennitt','Boss','Haters','ayn','Kgf','😎','Akshay uyir','sed bgm','Messi','Hehe','hehe','Set aano','set aano','Bot myren','Venda','venda','chadhi','Chadhi','Hbday','hbday','Bot','R yyi padicho','Myre','myre','Oompi','oompi','parayatte','Fresh','fresh','Ok da','ok da','Feel aayi','feel aaayi','scene','Ok bei','ok bei','Da','Kozhi','kozhi','adi','Adi','kali','Kali','thantha','Thantha','Aysheri','aysheri','thund','Thund','thot','Thot','Sneham','pm','Pm','paatt','Paatt','njan','Njan','life','Life','Killadi','killadi','good bye','Good bye','evide','Evide','achan','Achan','kunna','Kunna','broken','Broken','why','Why','enth patti','Enth patti','pani','Pani','padicho','Padicho','paad','Paad','Chatho','chatho','lover','Lover','nanayikoode','Nanayikoode','die','hate','Hate','Lamiya engineering','lamiya engineering','nallath','Nallath','Neymer','neymer','Njr','njr','patti','Patti','poora','Poora','Rohit','rohit','thall','Thall','Theri','theri','potte','Potte','Pinky','Caption','caption','onn poyi','Onn poyi','problem','Problem','Ameer','ameer','Suhail','Mathy','BGM','Die','Jd','Loveu','Veeran','Waiting','ano','ara','Ara','alone','bad boy','brokenlove','care','chunke','comedy','devadha','ekk','fd','free','gdmng','gdngt','group','ha','i am back','ijathi','jd','kadhal','kerivaa','kiss','kukku','line','lucifer','machan','Chetta','Per','massbgm','Mass','matam','may i','mindathe','my area','mylove','njn','padakam','polika','racal','rasool','rashmika','return','sneham','sulthan','thayirmulak','vada','wow','Thalapathy','Pikachu','Aliya','Leopucha','Poompatta gunda','Bgm','En nenjil','evde','Frnd','Game','Hy','Hoi','Yaar','Kadhali','king','Paavam','Pewer','pewer','Power','Pranayam','sed','Umma','Vava','Kunju','💪','🤣','Sis','Bairavaa','bgm','Love tune','Mohanlal','Singapenne','Single','Jocker','1 vs 1 vada','1','alive','Aliyo','Ardra','Area','Ayn','bot','Chathi','Chunk','Chunks','cr7','Cry','Dai','Dora','Entry bgm','Gd mng','Gd ngt','Hloo','Kanjan','Ikkachi','Kanjav','Kevin','Kundan','kundan','manath','messi','My love','Nalla kutty','Nanban','Nanni','Nishal','Njn vera','Oh','Ok','Over','Pala shaji','Pever','Pinnallah','Poli','Potte','Power varatte','Re entry','Sad','sad','T','Sahva','Scene','sed','Saji','Seth po','sis','Song','song','Supper','Vidhi','Baby','Music pranthan','Kenzo','kenzo','Kenzoo','Ashkar','per','Hbd','Mad','Blackzue','Nallakutti','Enthada','Akhil','Love','Fsq','Nanbiye','Iblees','Rashmika','Kutty','Arakkal iblees','Althaf','Sayip','thamasha','Edi','Muth','Muthe','Omb','2','Mood','Ok bye','Eda','Track maat','Chaya kudicho','photo','Set','Poote','Chathy','Aara','ayye','Ayyo','Bass bgm','Bgm id','Bhasi','Biscket','Chirikano','Chunnk uyr','ok','Umbi','da','Csk','ee','Ellarum ede','Etha cheyya','Entha','Fan','ff','grp','Kanapi','Kanaran','Kemam','kk','left','Lo','Loo','Lub u','muthe','My god','oombi','over','Paat','Panni','Podai','Pooda','Pora','Poyeda','Rasheed','Sed aayi','Sed tune','sed','Super','thayoli','Thug','Vaa','welcome','Wait']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
        await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
 }
 });
-
+    }
     var filtreler = await FilterDb.getFilter(message.jid);
     if (!filtreler) return; 
     filtreler.map(
@@ -75,11 +72,10 @@ if(pattern.test(message.message)){
         }
     );
 }));
-
-
+}
+else if (Config.WORKTYPE == 'public') {
 Asena.addCommand({pattern: 'filter ?(.*)', fromMe: true, desc: Lang.FILTER_DESC, dontAddCommandList: true}, (async (message, match) => {
     match = match[1].match(/[\'\"\“](.*?)[\'\"\“]/gsm);
-
     if (match === null) {
         filtreler = await FilterDb.getFilter(message.jid);
         if (filtreler === false) {
@@ -102,7 +98,6 @@ Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, don
     if (match === null) {
         return await message.client.sendMessage(message.jid,Lang.NEED_REPLY + '\n*Example:* ```.stop "hello"```',MessageType.text)
     }
-
     del = await FilterDb.deleteFilter(message.jid, match[0].replace(/['"“]+/g, ''));
     
     if (!del) {
@@ -111,12 +106,37 @@ Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC, don
         await message.client.sendMessage(message.jid,Lang.DELETED, MessageType.text)
     }
 }));
-
 Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
-    if(Config.BGMFILTER){
+        if(Config.BGMFILTER){
+        let banned = jid.find( Jid => Jid === message.jid);
+        if(banned !== undefined) return
+        if (!!message.mention && message.mention[0] == '919946432377@s.whatsapp.net') {
+await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/Mention.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
+        }
+const array = ['Helo','King','Kooi','Ramos','Tentacion','Neymar','umma','Music','Kurup','Rose','aara','Alone','ayilla','bie','Chiri','colony','enth','entha','Fuck','Goal','Hambada','Kanja','kuthirappavan','mathi','mier','moonji','pever','Potta','Serious','Soldier','Sry','Subscribe','thottu','Va','Vada','vimanam','sorry','nanban','Lala','Smile','ghost','La be','Sed','Uff','Legend','music','Fek','Psycho','Town','Pwoli','Uyir','Malang','Bad','Boss','Thamasha','big fan','gd n8','kar98','love u','Love u','Endi','endi','noob','Noob','Poweresh','Perfect ok','perfect ok','power','saji','single','waiting','Myr','myr','uyir','thug','avastha','Moodesh','sketched','Cr7','manasilayo','Hlo','Poda','Aarulle','Cr7 back','Portugal','ennitt','Haters','ayn','Kgf','Messi','Hehe','hehe','Venda','venda','chadhi','Chadhi','Hbday','hbday','Myre','myre','Oompi','oompi','parayatte','Fresh','fresh','Ok da','ok da','Feel aayi','scene','Ok bei','ok bei','Da','Kozhi','kozhi','adi','Adi','kali','Kali','thantha','Thantha','Aysheri','aysheri','thot','Thot','Sneham','pm','Pm','paatt','Paatt','njan','Njan','life','Life','Killadi','killadi','good bye','Good bye','evide','Evide','achan','Achan','kunna','Kunna','broken','Broken','why','Why','enth patti','Enth patti','pani','Pani','padicho','Padicho','paad','Paad','Chatho','chatho','lover','Lover','nanayikoode','Nanayikoode','die','hate','Hate','nallath','Nallath','Neymer','neymer','Njr','njr','patti','Patti','poora','Poora','Rohit','rohit','thall','Thall','Theri','theri','potte','Potte','Pinky','Caption','caption','onn poyi','Onn poyi','problem','Problem','Ameer','ameer','Suhail','Mathy','BGM','Die','Jd','Loveu','Veeran','Waiting','ano','ara','Ara','alone','bad boy','brokenlove','care','chunke','comedy','devadha','ekk','fd','free','gdmng','gdngt','group','ha','i am back','ijathi','jd','kadhal','kerivaa','kiss','kukku','line','lucifer','machan','Chetta','Per','massbgm','Mass','matam','may i','mindathe','my area','mylove','njn','padakam','polika','racal','rasool','rashmika','return','sneham','sulthan','thayirmulak','vada','wow','Thalapathy','Aliya','Leopucha','Poompatta gunda','Bgm','En nenjil','evde','Frnd','Game','Hy','Hoi','Yaar','Kadhali','king','Paavam','Pewer','pewer','Power','Pranayam','sed','Umma','Vava','Kunju','💪','🤣','Sis','Bairavaa','bgm','Love tune','Mohanlal','Singapenne','Single','Jocker','1 vs 1 vada','1','alive','Aliyo','Ardra','Area','Ayn','bot','Chathi','Chunk','Chunks','cr7','Cry','Dai','Dora','Entry bgm','Gd mng','Gd ngt','Hloo','Kanjan','Ikkachi','Kanjav','Kevin','Kundan','kundan','manath','messi','My love','Nalla kutty','Nanban','Nanni','Nishal','Njn vera','Oh','Ok','Over','Pala shaji','Pever','Pinnallah','Poli','Potte','Power varatte','Re entry','Sad','sad','T','Sahva','Scene','sed','Saji','Seth po','sis','Supper','Vidhi','Baby','Music pranthan','Kenzo','kenzo','Kenzoo','Ashkar','per','Hbd','Mad','Blackzue','Nallakutti','Enthada','Akhil','Love','Fsq','Nanbiye','Iblees','Rashmika','Kutty','Arakkal iblees','Althaf','Sayip','thamasha','Edi','Muth','Muthe','Omb','2','Mood','Ok bye','Eda','Track maat','Chaya kudicho','photo','Set','Poote','Chathy','Aara','ayye','Ayyo','Bass bgm','Bgm id','Bhasi','Biscket','Chirikano','Chunnk uyr','ok','Umbi','da','Csk','ee','Ellarum ede','Etha cheyya','Entha','Fan','ff','grp','Kanapi','Kanaran','Kemam','kk','left','Lo','Loo','Lub u','muthe','My god','oombi','over','Paat','Panni','Podai','Pooda','Pora','Poyeda','Rasheed','Sed aayi','Sed tune','sed','Super','thayoli','Thug','Vaa','nandu','Nandu','welcome','Me','Wait']
+array.map( async (a) => {
+let pattern = new RegExp(`\\b${a}\\b`, 'g');
+if(pattern.test(message.message)){
+       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
+}
+});
+    }
+    var filtreler = await FilterDb.getFilter(message.jid);
+    if (!filtreler) return; 
+    filtreler.map(
+        async (filter) => {
+            pattern = new RegExp(filter.dataValues.regex ? filter.dataValues.pattern : ('\\b(' + filter.dataValues.pattern + ')\\b'), 'gm');
+            if (pattern.test(message.message)) {
+                await message.client.sendMessage(message.jid,filter.dataValues.text, MessageType.text, {quoted: message.data});
+            }
+        }
+    );
+}));
+Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
+    if(Config.AUTOSTICKER){
     let banned = jid.find( Jid => Jid === message.jid);
     if(banned !== undefined) return
-    if (!!message.mention && message.mention[0] == '918921483992@s.whatsapp.net') {
+    if (!!message.mention && message.mention[0] == '919946432377@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./sticker/song.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
 const array = ['Pikachu','Msd','Vijay','Rashmika','song','Sry','Da','Line']
@@ -127,7 +147,6 @@ if(pattern.test(message.message)){
 }
 });
 }
-
 var filtreler = await FilterDb.getFilter(message.jid);
 if (!filtreler) return; 
 filtreler.map(
@@ -139,3 +158,4 @@ filtreler.map(
     }
 );
 }));
+}
